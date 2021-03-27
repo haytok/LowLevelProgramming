72b0613324cf44d4d6a711e031d31a68b71ae8d4
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-03-28 03:20:12 +0900

    add

--- stdlinux2-source/ch8/README.md
+++ stdlinux2-source/ch8/README.md
@@ -11,24 +11,74 @@
 - regcomp (man 3 regcomp)
 
 ```bash
+SYNOPSIS
+       #include <sys/types.h>
+       #include <regex.h>
+
+       int regcomp(regex_t *preg, const char *regex, int cflags);
+
+DESCRIPTION
+   POSIX regex compiling
+       regcomp()  is used to compile a regular expression into a form that is suit‐
+       able for subsequent regexec() searches.
+
+       regcomp() is supplied with preg, a pointer to a pattern buffer storage area;
+       regex,  a  pointer  to  the null-terminated string and cflags, flags used to
+       determine the type of compilation.
+
+RETURN VALUE
+       regcomp()  returns  zero  for  a successful compilation or an error code for
+       failure.
 ```
 
 - regfree
 
 ```bash
+SYNOPSIS
+       void regfree(regex_t *preg);
+
+DESCRIPTION
+       Supplying regfree() with a precompiled pattern buffer, preg  will  free  the
+       memory allocated to the pattern buffer by the compiling process, regcomp().
 ```
 
 - regexec
 
 ```bash
+SYNOPSIS
+       int regexec(const regex_t *preg, const char *string, size_t nmatch,
+                   regmatch_t pmatch[], int eflags);
+
+DESCRIPTION
+   POSIX regex matching
+       regexec() is used to match a null-terminated string against the  precompiled
+       pattern  buffer,  preg.   nmatch  and pmatch are used to provide information
+       regarding the location of any matches.  eflags may be the bitwise-or of  one
+       or  both of REG_NOTBOL and REG_NOTEOL which cause changes in matching behav‐
+       ior described below.
+
+RETURN VALUE
+       regexec() returns zero for a successful match or REG_NOMATCH for failure.
 ```
 
 - regerror
 
 ```bash
+SYNOPSIS
+       void regfree(regex_t *preg);
+
+DESCRIPTION
+       regerror()  is  passed  the error code, errcode, the pattern buffer, preg, a
+       pointer to a character string buffer, errbuf, and the  size  of  the  string
+       buffer,  errbuf_size.  It returns the size of the errbuf required to contain
+       the null-terminated error message string.  If both  errbuf  and  errbuf_size
+       are  nonzero,  errbuf is filled in with the first errbuf_size - 1 characters
+       of the error message and a terminating null byte ('\0').
 ```
 
 ## テスト方法
+- `gcc -o grep grep.c && cat grep.c | ./grep argc`
+- `gcc -o grep grep.c | ./grep argc grep.c`
 
 ## この章の流れ
 
--- stdlinux2-source/ch8/grep.c
+++ stdlinux2-source/ch8/grep.c
@@ -1,6 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <regex.h>
+#include <getopt.h>
+
+void do_grep(regex_t *pattern, FILE *file);
+
+static struct option long_options[] = {
+    {"ignore-case", no_argument, 0, 'i'},
+    {"invert-match", no_argument, 0, 'v'},
+    {"help", no_argument, 0, 'h'},
+    {0, 0, 0, 0}
+};
+int opt_invert = 0;
 
 int main(int argc, char **argv) {
-    printf("Hello\n");
+    if (argc < 2) {
+        fputs("no pattern\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+    // オプションの解析
+    int c;
+    int option_index = 0;
+    int cflags = REG_EXTENDED | REG_NOSUB | REG_NEWLINE;
+    while (
+        (c = getopt_long(argc, argv, "ivh", long_options, &option_index)) != -1
+    ) {
+        switch (c) {
+            case 'i': {
+                cflags |= REG_ICASE;
+                break;
+            }
+            case 'v': {
+                opt_invert = 1;
+                break;
+            }
+            case 'h': {
+                fprintf(stdout, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
+                exit(EXIT_SUCCESS);
+            }
+            case '?': {
+                fprintf(stderr, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    regex_t pattern;
+    printf("argc %d, optind %d %s, %d %s\n", argc, optind, argv[optind], optind+1, argv[optind+1]);
+    printf("ARGC");
+    argc -= optind;
+    argv += optind;
+    int err;
+    err = regcomp(&pattern, argv[0], cflags);
+    if (err != 0) {
+        char buf[1024];
+        regerror(err, &pattern, buf, sizeof(buf));
+        puts(buf);
+        exit(EXIT_FAILURE);
+    }
+    argc--;
+    if (argc == 0) {
+        // 引数にそのまま pattern を入れるとエラーが生じる。
+        // do_grep が求めるのは *pattern である。
+        do_grep(&pattern, stdin);
+    } else {
+        for (int i = optind; i < argc; i++) {
+            char *path = argv[i];
+            FILE *file = fopen(path, "r");
+            if (!file) {
+                perror(path);
+                exit(EXIT_FAILURE);
+            }
+            do_grep(&pattern, file);
+            fclose(file);
+        }
+    }
+}
+
+// cat grep.c | grep <正規表現>
+void do_grep(regex_t *pattern, FILE *file) {
+    char buf[4096];
+    int matched;
+    while (fgets(buf, sizeof(buf), file) != NULL) {
+        matched = (regexec(pattern, buf, 0, NULL, 0) == 0);
+        if (opt_invert) {
+            matched = !matched;
+        }
+        if (matched) {
+            puts(buf);
+        }
+    }
 }
