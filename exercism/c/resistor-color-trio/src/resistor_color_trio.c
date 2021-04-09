3b2b5f42d213f94526007f1256d1b2ae8cbefcbc
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-09 21:02:58 +0900

    solve

--- exercism/c/resistor-color-trio/src/resistor_color_trio.c
+++ exercism/c/resistor-color-trio/src/resistor_color_trio.c
@@ -1,1 +1,13 @@
 #include "resistor_color_trio.h"
+
+resistor_value_t color_code(resistor_band_t *bands) {
+    resistor_value_t actual;
+    actual.value = (bands[0] * 10 + bands[1]) * pow(10, bands[2]);
+    if (bands[2] < RED) {
+        actual.unit = OHMS;
+    } else {
+        actual.unit = KILOOHMS;
+        actual.value /= 1000;
+    }
+    return actual;
+}
--- exercism/c/resistor-color-trio/src/resistor_color_trio.h
+++ exercism/c/resistor-color-trio/src/resistor_color_trio.h
@@ -1,4 +1,36 @@
 #ifndef RESISTOR_COLOR_TRIO_H
 #define RESISTOR_COLOR_TRIO_H
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct resistor_value_t resistor_value_t;
+
+typedef enum {
+    BLACK,
+    BROWN,
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    VIOLET,
+    GREY,
+    WHITE,
+} resistor_band_t;
+
+typedef enum {
+    OHMS,
+    KILOOHMS,
+} register_unit_t;
+
+struct resistor_value_t {
+    int value;
+    register_unit_t unit;
+};
+
+resistor_value_t color_code(resistor_band_t *bands);
+
 #endif
