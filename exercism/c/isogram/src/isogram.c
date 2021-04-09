34759c3b6455a3f5fb9fb57d2bd795df9b9a08bb
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-09 22:58:00 +0900

    solve

--- exercism/c/isogram/src/isogram.c
+++ exercism/c/isogram/src/isogram.c
@@ -1,1 +1,16 @@
 #include "isogram.h"
+
+bool is_isogram(const char phrase[]) {
+    if (!phrase) {
+        return false;
+    }
+    for (u_int i = 0; i < strlen(phrase); i++) {
+        char string = tolower(phrase[i]);
+        for (u_int j = 0; j < strlen(phrase); j++) {
+            if (isalpha(string) && j != i && tolower(phrase[j]) == string) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
--- exercism/c/isogram/src/isogram.h
+++ exercism/c/isogram/src/isogram.h
@@ -1,8 +1,15 @@
 #ifndef ISOGRAM_H
 #define ISOGRAM_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
+
+typedef unsigned int u_int;
 
 bool is_isogram(const char phrase[]);
+bool is_exist(const char string, char d[255]);
 
 #endif
