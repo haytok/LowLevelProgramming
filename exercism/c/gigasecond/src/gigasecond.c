faad7e1d5772c6e5777fa6e3221eb8963d8c2973
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-11 04:37:07 +0900

    solve

--- exercism/c/gigasecond/src/gigasecond.c
+++ exercism/c/gigasecond/src/gigasecond.c
@@ -1,1 +1,6 @@
 #include "gigasecond.h"
+
+time_t gigasecond_after(time_t t) {
+    t += 1000000000;
+    return t;
+}
--- exercism/c/gigasecond/src/gigasecond.h
+++ exercism/c/gigasecond/src/gigasecond.h
@@ -2,7 +2,12 @@
 #define GIGASECOND_H
 
 #include <time.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-time_t gigasecond_after(time_t);
+time_t gigasecond_after(time_t t);
 
 #endif
