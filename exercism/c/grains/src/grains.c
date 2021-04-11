d5016c6c99d4f89d7b2c5a6d2a3e8dd777a8529c
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-12 03:14:56 +0900

    solve

--- exercism/c/grains/src/grains.c
+++ exercism/c/grains/src/grains.c
@@ -1,1 +1,16 @@
 #include "grains.h"
+
+uint64_t square(uint8_t index) {
+    if (index > 64) {
+        return 0;
+    }
+    return pow(2, (index - 1));
+}
+
+uint64_t total(void) {
+    uint64_t counts = 0;
+    for (int i = 1; i < 65; i++) {
+        counts += square(i);
+    }
+    return counts;
+}
--- exercism/c/grains/src/grains.h
+++ exercism/c/grains/src/grains.h
@@ -2,6 +2,13 @@
 #define GRAINS_H
 
 #include <stdint.h>
+#include <time.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
 
 uint64_t square(uint8_t index);
 uint64_t total(void);
