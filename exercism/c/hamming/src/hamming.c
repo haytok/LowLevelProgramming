3e4d963444899f8b3e627bbbf5989135bcdecd18
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-12 01:30:34 +0900

    slove

--- exercism/c/hamming/src/hamming.c
+++ exercism/c/hamming/src/hamming.c
@@ -1,1 +1,16 @@
 #include "hamming.h"
+
+int compute(const char *lhs, const char *rhs) {
+    if (strlen(lhs) != strlen(rhs)) {
+        return -1;
+    }
+    int counts = 0;
+    while (*lhs && *rhs) {
+        if (strncmp(lhs, rhs, 1) != 0) {
+            counts++;
+        }
+        lhs++;
+        rhs++;
+    }
+    return counts;
+}
--- exercism/c/hamming/src/hamming.h
+++ exercism/c/hamming/src/hamming.h
@@ -1,6 +1,12 @@
 #ifndef HAMMING_H
 #define HAMMING_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+
 int compute(const char *lhs, const char *rhs);
 
 #endif
