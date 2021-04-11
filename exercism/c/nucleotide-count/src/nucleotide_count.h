ed24eabe30815d9a108899cee5550102b7205377
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-12 02:18:07 +0900

    setup

---
+++ exercism/c/nucleotide-count/src/nucleotide_count.h
@@ -0,0 +1,13 @@
+#ifndef _NUCLEOTIDE_COUNT_H
+#define _NUCLEOTIDE_COUNT_H
+
+#include <time.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+char *count(const char *dna_strand);
+
+#endif
