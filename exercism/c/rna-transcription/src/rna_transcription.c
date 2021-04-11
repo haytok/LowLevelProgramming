e83e9e2620033fdb053c4c7c6af25109eacd3a3c
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-12 02:16:55 +0900

    solve

--- exercism/c/rna-transcription/src/rna_transcription.c
+++ exercism/c/rna-transcription/src/rna_transcription.c
@@ -1,3 +1,25 @@
 #include "rna_transcription.h"
 
-char *to_rna(const char *dna) {}
+// 入力として与えられる dna をmemcpy する。
+// コピーした先の値を一つずつ見ていき、直に書き換えていく。
+char *to_rna(const char *dna) {
+    char *rna;
+    rna = malloc(sizeof(char *) * 1024);
+    memcpy(rna, dna, 1024);
+
+    char *p;
+    p  = malloc(sizeof(char *) * 1024);
+    for (p = rna; *p; p++) {
+        *p = (char)toupper((int)*p);
+        if (*p == 'A') {
+            *p = 'U';
+        } else if (*p == 'T') {
+            *p = 'A';
+        } else if (*p == 'G') {
+            *p = 'C';
+        } else if (*p == 'C') {
+            *p = 'G';
+        }
+    }
+    return rna;
+}
