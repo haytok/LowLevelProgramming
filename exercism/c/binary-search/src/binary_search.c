3d2581de0d354cf96c8dddfa84cf99b40a54c5ff
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-14 03:03:30 +0900

    solve

--- exercism/c/binary-search/src/binary_search.c
+++ exercism/c/binary-search/src/binary_search.c
@@ -1,1 +1,19 @@
 #include "binary_search.h"
+
+int *binary_search(int value, int *arr, size_t length) {
+    int left, right, middle;
+    left = 0;
+    right = length - 1;
+    while (left <= right) {
+        middle = (left + right) / 2;
+        if (arr[middle] == value) {
+            printf("middle %d:\n", middle);
+            return &arr[middle];
+        } else if (arr[middle] < value) {
+            left = middle + 1;
+        } else if (arr[middle] > value) {
+            right = middle - 1;
+        }
+    }
+    return NULL;
+}
--- exercism/c/binary-search/src/binary_search.h
+++ exercism/c/binary-search/src/binary_search.h
@@ -2,7 +2,15 @@
 #define BINARY_SEARCH_H
 
 #include <stddef.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <time.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-int *binary_search(int value, const int *arr, size_t length);
+int *binary_search(int value, int *arr, size_t length);
 
 #endif
