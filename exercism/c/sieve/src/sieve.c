726c6044aa2848b4943fa53e87b001ea89e04a24
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-16 23:53:34 +0900

    solve

--- exercism/c/sieve/src/sieve.c
+++ exercism/c/sieve/src/sieve.c
@@ -1,1 +1,23 @@
 #include "sieve.h"
+
+uint32_t sieve(uint32_t limit, uint32_t * primes, size_t max_primes) {
+    int count = 0;
+    if (limit == 1) {
+        return (uint32_t)count;
+    }
+    for (int i = 2; i <= (int)limit; i++) {
+        if (count < (int)max_primes && is_primary(i)) {
+            primes[count++] = (uint32_t)i;
+        }
+    }
+    return (uint32_t)count;
+}
+
+bool is_primary(int value) {
+  for(int i = 2; i < value ; ++i) {
+    if(value % i == 0 ) {
+        return false;
+    }
+  }
+  return true;
+}
--- exercism/c/sieve/src/sieve.h
+++ exercism/c/sieve/src/sieve.h
@@ -1,13 +1,21 @@
 #ifndef SIEVE_H
 #define SIEVE_H
 
-#include <stdint.h>
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdint.h>
+#include <math.h>
 
 /// Calculate at most `max_primes` prime numbers in the interval [2,limit] 
 /// using the Sieve of Eratosthenes and store the prime numbers in `primes`
 /// in increasing order.
 /// The function returns the number of calculate primes.
 uint32_t sieve(uint32_t limit, uint32_t * primes, size_t max_primes);
+bool is_primary(int value);
 
 #endif
