4411308bed279cb8cb54253b3a17c38474277e6b
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-17 04:46:28 +0900

    solve

--- exercism/c/triangle/src/triangle.c
+++ exercism/c/triangle/src/triangle.c
@@ -1,1 +1,58 @@
 #include "triangle.h"
+
+// typedef struct {
+//    double a;
+//    double b;
+//    double c;
+// } triangle_t;
+
+bool is_equilateral(triangle_t sides) {
+    if (sides.a == 0 && sides.b == 0 && sides.c == 0) {
+        return false;
+    }
+    if (sides.a == sides.b && sides.b == sides.c) {
+        return true;
+    }
+    return false;
+}
+
+bool is_isosceles(triangle_t sides) {
+    if (sides.a == sides.b) {
+        return is_triangle(sides.a, sides.c);
+    } else if (sides.b == sides.c) {
+        return is_triangle(sides.b, sides.a);
+    } else if (sides.c == sides.a) {
+        return is_triangle(sides.c, sides.b);
+    }
+    return false;
+}
+
+bool is_triangle(double x, double y) {
+    return 2 * x > y ? true : false;
+}
+
+bool is_scalene(triangle_t sides) {
+    if (is_exist(sides)) {
+        if (is_equilateral(sides) || is_isosceles(sides)) {
+            return false;
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool is_exist(triangle_t sides) {
+    if (
+        is_triple(sides.a, sides.b, sides.c)
+        && is_triple(sides.b, sides.c, sides.a)
+        && is_triple(sides.c, sides.a, sides.b)
+    ) {
+        return true;
+    }
+    return false;
+}
+
+bool is_triple(double x, double y, double z) {
+    return x + y > z ? true : false;
+}
--- exercism/c/triangle/src/triangle.h
+++ exercism/c/triangle/src/triangle.h
@@ -1,10 +1,27 @@
 #ifndef TRIANGLE_H
 #define TRIANGLE_H
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdint.h>
+
 typedef struct {
    double a;
    double b;
    double c;
 } triangle_t;
+
+bool is_equilateral(triangle_t sides);
+bool is_isosceles(triangle_t sides);
+bool is_scalene(triangle_t sides);
+bool is_triangle(double x, double y);
+bool is_exist(triangle_t sides);
+bool is_triple(double x, double y, double z);
 
 #endif
