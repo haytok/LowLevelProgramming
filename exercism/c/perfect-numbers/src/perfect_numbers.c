c63a36532a616905211f82574b81a3aba269951d
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-17 15:31:34 +0900

    solve

--- exercism/c/perfect-numbers/src/perfect_numbers.c
+++ exercism/c/perfect-numbers/src/perfect_numbers.c
@@ -1,1 +1,21 @@
 #include "perfect_numbers.h"
+
+kind classify_number(int value) {
+    if (value <= 0) {
+        return ERROR;
+    }
+    int sum = 0;
+    for (int i = 1; i < value; ++i) {
+        if ( value % i == 0 ) {
+            sum += i;
+        }
+    }
+    if (sum == value) {
+        return PERFECT_NUMBER;
+    } else if (sum > value) {
+        return ABUNDANT_NUMBER;
+    } else if (sum < value) {
+        return DEFICIENT_NUMBER;
+    }
+    return ERROR;
+}
