5aebbbcdd96022ed4f259e221824f33c8f02ed1e
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-17 15:59:09 +0900

    solve

--- exercism/c/binary/src/binary.c
+++ exercism/c/binary/src/binary.c
@@ -1,1 +1,24 @@
 #include "binary.h"
+
+int convert(const char *input) {
+    int ans, index;
+    char *head;
+
+    ans = 0;
+    index = strlen(input) - 1;
+    head = malloc(sizeof(char) * 1024);
+    memcpy(head, input, strlen(input));
+
+    while (*head) {
+        char *buf = malloc(sizeof(char) * 1);
+        strncpy(buf, head, 1);
+        if (strcmp(buf, "2") == 0 || isalpha(*buf) != 0) {
+            return INVALID;
+        }
+        ans += (int)pow(2, (index)) * atoi(buf);
+        index--;
+        head++;
+    }
+
+    return ans;
+}
--- exercism/c/binary/src/binary.h
+++ exercism/c/binary/src/binary.h
@@ -1,6 +1,16 @@
 #ifndef BINARY_H
 #define BINARY_H
 
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdint.h>
+#include <math.h>
+
 #define INVALID -1
 
 int convert(const char *input);
