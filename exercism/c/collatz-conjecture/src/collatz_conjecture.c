b8c84bfc706fdeaf66d8caefe82138b34aa9cc7b
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-17 15:41:24 +0900

    solve

--- exercism/c/collatz-conjecture/src/collatz_conjecture.c
+++ exercism/c/collatz-conjecture/src/collatz_conjecture.c
@@ -1,1 +1,13 @@
 #include "collatz_conjecture.h"
+
+int steps(int start) {
+    if (start <= 0) {
+        return ERROR_VALUE;
+    }
+    int count = 0;
+    while (start != 1) {
+        start = start % 2 == 0 ? start / 2 : start * 3 + 1;
+        count++;
+    }
+    return count;
+}
--- exercism/c/collatz-conjecture/src/collatz_conjecture.h
+++ exercism/c/collatz-conjecture/src/collatz_conjecture.h
@@ -1,6 +1,15 @@
 #ifndef COLLATZ_CONJECTURE_H
 #define COLLATZ_CONJECTURE_H
 
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdint.h>
+
 #define ERROR_VALUE -1
 
 int steps(int start);
