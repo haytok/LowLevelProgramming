c53afc50def740c58b9d57aa3195658e318f5f2d
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-19 22:51:01 +0900

    solve

--- exercism/c/secret-handshake/src/secret_handshake.c
+++ exercism/c/secret-handshake/src/secret_handshake.c
@@ -1,1 +1,35 @@
 #include "secret_handshake.h"
+
+const char *handshakes[] = {"wink", "double blink", "close your eyes", "jump"};
+
+const char **commands(size_t number) {
+    const char **ans;
+    int index;
+    ans = malloc(sizeof(char *) * 1024);
+    index = 0;
+    if (number == 0) {
+        ans[index] = NULL;
+        return ans;
+    }
+    for (int i = 0; number > 0; i++) {
+        int value = number % 2;
+        number = number / 2;
+        if (value == 0) {
+            continue;
+        } else if (i == 4) {
+            if (index == 0) {
+                ans[index] = NULL;
+                return ans;
+            }
+            for (int j = 0; j < index / 2; j++) {
+                const char *tmp;
+                tmp = ans[j];
+                ans[j] = ans[index - j - 1];
+                ans[index - j - 1] = tmp;
+            }
+        } else if (value == 1) {
+            ans[index++] = handshakes[i];
+        }
+    }
+    return ans;
+}
--- exercism/c/secret-handshake/src/secret_handshake.h
+++ exercism/c/secret-handshake/src/secret_handshake.h
@@ -2,6 +2,15 @@
 #define SECRET_HANDSHAKE_H
 
 #include <stddef.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdint.h>
 
 const char **commands(size_t number);
 
