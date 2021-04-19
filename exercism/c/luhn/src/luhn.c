eb2324b25ae80277bfc2f7906f00aa8eef151275
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-19 19:23:03 +0900

    solve

--- exercism/c/luhn/src/luhn.c
+++ exercism/c/luhn/src/luhn.c
@@ -1,1 +1,56 @@
 #include "luhn.h"
+
+bool luhn(const char *num) {
+    if (strcmp(num, "0") == 0) {
+        return false;
+    }
+    int i = 0;
+    int ans = 0;
+    char *head;
+    head = malloc(sizeof(char) * 1024);
+    strcpy(head, num);
+    rev_string(head);
+    while (*head) {
+        // 一文字ずつ index を使って処理をしても解ける。
+        // printf("In while, %c\n", num[i]);
+        char *tmp;
+        tmp = malloc(sizeof(char));
+        strncpy(tmp, head, 1);
+        if (isalpha(*tmp) != 0) {
+            return false;
+        }
+        if (strcmp(tmp, " ") == 0) {
+            head++;
+            continue;
+        }
+        int value = atoi(tmp);
+        if (i % 2 == 1) {
+            ans += (value * 2 > 9) ? value * 2 - 9 : value * 2;
+        } else {
+            ans += value;
+        }
+        head++;
+        i++;
+    }
+    if (i == 1 && ans == 0) {
+        return false;
+    }
+    return ans % 10 == 0 ? true : false;
+}
+
+unsigned str_length(const char str[]) {
+    unsigned len = 0;
+    while (str[len])
+        len++;
+    return (len);
+}
+
+void rev_string(char str[]) {
+    int i;
+    int len = str_length(str);
+    for (i = 0; i < len / 2; i++) {
+        char temp = str[i];
+        str[i] = str[len-i-1];
+        str[len-i-1] = temp;
+    }
+}
--- exercism/c/luhn/src/luhn.h
+++ exercism/c/luhn/src/luhn.h
@@ -2,7 +2,17 @@
 #define LUHN_H
 
 #include <stdbool.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdint.h>
 
 bool luhn(const char *num);
+unsigned str_length(const char str[]);
+void rev_string(char str[]);
 
 #endif
