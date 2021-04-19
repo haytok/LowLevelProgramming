e3aafd317e7b9c383209a70d543a7ce89d5b7544
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-20 02:05:39 +0900

    solve

--- exercism/c/diamond/src/diamond.c
+++ exercism/c/diamond/src/diamond.c
@@ -1,1 +1,35 @@
 #include "diamond.h"
+
+char **make_diamond(const char letter) {
+    char base = 'A';
+    int diff = letter - 'A';
+    int length = diff * 2 + 1;
+    char **ans;
+    ans = malloc(sizeof(char *) * length);
+    for (int row = 0; row < length; row++) {
+        ans[row] = malloc(sizeof(char *) * length);
+        for (int column = 0; column < length; column++) {
+            if (row == 0 || row == length - 1) {
+                if (column == letter - 'A') {
+                    ans[row][column] = base;
+                } else {
+                    ans[row][column] = ' ';
+                }
+            } else {
+                if (column == diff || column == length - 1 - diff) {
+                    ans[row][column] = base;
+                } else {
+                    ans[row][column] = ' ';
+                }
+            }
+        }
+        if (row >= 0 && row < letter - 'A') {
+            base++;
+            diff--;
+        } else {
+            base--;
+            diff++;
+        }
+    }
+    return ans;
+}
--- exercism/c/diamond/src/diamond.h
+++ exercism/c/diamond/src/diamond.h
@@ -1,6 +1,15 @@
 #ifndef DIAMOND_H
 #define DIAMOND_H
 
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdint.h>
+
 char **make_diamond(const char letter);
 
 #endif
