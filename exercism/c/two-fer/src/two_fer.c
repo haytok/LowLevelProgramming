a5182f55f844348e67d4e598254bbeec8d22f55a
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-20 01:31:18 +0900

    solve

--- exercism/c/two-fer/src/two_fer.c
+++ exercism/c/two-fer/src/two_fer.c
@@ -1,1 +1,6 @@
 #include "two_fer.h"
+
+void two_fer(char *buffer, const char *name) {
+    char *fmt = "One for %s, one for me.";
+    sprintf(buffer, fmt, name == NULL ? "you" : name);
+}
--- exercism/c/two-fer/src/two_fer.h
+++ exercism/c/two-fer/src/two_fer.h
@@ -1,6 +1,15 @@
 #ifndef TWO_FER_H
 #define TWO_FER_H
 
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdint.h>
+
 void two_fer(char *buffer, const char *name);
 
 #endif
