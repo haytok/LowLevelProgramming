941ff3f8e9c9b8172c874af410a2a9c9d4c6f2af
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-21 05:39:27 +0900

    solve

--- exercism/c/sum-of-multiples/src/sum_of_multiples.c
+++ exercism/c/sum-of-multiples/src/sum_of_multiples.c
@@ -1,1 +1,27 @@
 #include "sum_of_multiples.h"
+
+unsigned int sum(const unsigned int *factors,
+                 const size_t number_of_factors, const unsigned int limit) {
+    int under_ans = 0;
+    int ans = 0;
+    bool flag = false;
+    for (int i = 0; i < (int)number_of_factors; i++) {
+        if (factors[i] == 0) {
+            flag = true;
+        } else {
+            under_ans += factors[i];
+        }
+    }
+    if (flag) {
+        return under_ans;
+    }
+    for (int j = 0; j < (int)limit; j++) {
+        for (int k = 0; k < (int)number_of_factors; k++) {
+            if (j % factors[k] == 0) {
+                ans += j;
+                break;
+            }
+        }
+    }
+    return ans;
+}
--- exercism/c/sum-of-multiples/src/sum_of_multiples.h
+++ exercism/c/sum-of-multiples/src/sum_of_multiples.h
@@ -1,7 +1,17 @@
 #ifndef SUM_OF_MULTIPLES_H
 #define SUM_OF_MULTIPLES_H
 
+#define LENGTH 32
+
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdint.h>
 
 unsigned int sum(const unsigned int *factors,
                  const size_t number_of_factors, const unsigned int limit);
