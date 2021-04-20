41d1537abbead12208e8a1852cc2ea1ff9b1a568
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-21 05:49:10 +0900

    solve

--- exercism/c/square-root/src/square_root.c
+++ exercism/c/square-root/src/square_root.c
@@ -1,1 +1,5 @@
 #include "square_root.h"
+
+int square_root(int value) {
+    return (int)sqrt(value);
+}
--- exercism/c/square-root/src/square_root.h
+++ exercism/c/square-root/src/square_root.h
@@ -1,4 +1,16 @@
 #ifndef SQUARE_ROOT_H
 #define SQUARE_ROOT_H
 
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdint.h>
+#include <math.h>
+
+int square_root(int value);
+
 #endif
