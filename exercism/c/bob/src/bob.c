ae1942053f1c946e82db66a67b29b14951113754
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-20 22:25:52 +0900

    solve

--- exercism/c/bob/src/bob.c
+++ exercism/c/bob/src/bob.c
@@ -1,1 +1,37 @@
 #include "bob.h"
+
+char *hey_bob(char *greeting) {
+    bool is_question = false;
+    bool is_lower = false;
+    bool is_alpha = false;
+    bool is_silence = true;
+    while (*greeting) {
+        if (isalpha(*greeting)) {
+            is_alpha = true;
+        }
+        if (islower(*greeting)) {
+            is_lower = true;
+        }
+        if (!isspace(*greeting)) {
+            is_silence = false;
+            is_question = false;
+        }
+        if (*greeting == '?') {
+            is_question = true;
+        }
+        greeting++;
+    }
+    if (is_alpha && !is_lower && is_question) {
+        return "Calm down, I know what I'm doing!";
+    }
+    if (is_alpha && !is_lower) {
+        return "Whoa, chill out!";
+    }
+    if (is_question) {
+        return "Sure.";
+    }
+    if (is_silence) {
+        return "Fine. Be that way!";
+    }
+    return "Whatever.";
+}
--- exercism/c/bob/src/bob.h
+++ exercism/c/bob/src/bob.h
@@ -1,6 +1,15 @@
 #ifndef BOB_H
 #define BOB_H
 
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdint.h>
+
 char *hey_bob(char *greeting);
 
 #endif
