e792bd28ebeb94c0cd89e2f104a2f78579f44a8a
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-22 00:36:50 +0900

    solve

--- exercism/c/pythagorean-triplet/src/pythagorean_triplet.c
+++ exercism/c/pythagorean-triplet/src/pythagorean_triplet.c
@@ -1,1 +1,25 @@
 #include "pythagorean_triplet.h"
+
+triplets_t *triplets_with_sum(uint16_t sum) {
+    triplets_t *triplets;
+    triplets = malloc(sizeof(triplets_t));
+    triplets->count = 0;
+    triplets->triplets = malloc(sizeof(triplets) * 1024);
+    for (int a = 0; a < (int)sum; a++) {
+        for (int b = a + 1; b < (int)sum; b++) {
+            int c = sum - a - b;
+            if (b >= c) {
+                continue;
+            }
+            if (pow(a, 2) + pow(b, 2) == pow(c, 2)) {
+                triplets->triplets[triplets->count++] = (triplet_t){.a=a, .b=b, .c=c};
+            }
+        }
+    }
+    return triplets;
+}
+
+void free_triplets(triplets_t *triplets) {
+    free(triplets->triplets);
+    free(triplets);
+}
--- exercism/c/pythagorean-triplet/src/pythagorean_triplet.h
+++ exercism/c/pythagorean-triplet/src/pythagorean_triplet.h
@@ -1,4 +1,33 @@
 #ifndef PYTHAGOREAN_TRIPLET
 #define PYTHAGOREAN_TRIPLET
 
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdint.h>
+#include <math.h>
+
+#define LENGTH 3
+
+typedef struct triplet_t triplet_t;
+typedef struct triplets_t triplets_t;
+
+struct triplet_t {
+    int a;
+    int b;
+    int c;
+};
+
+struct triplets_t {
+    int count;
+    triplet_t *triplets;
+};
+
+triplets_t *triplets_with_sum(uint16_t sum);
+void free_triplets(triplets_t *triplets);
+
 #endif
