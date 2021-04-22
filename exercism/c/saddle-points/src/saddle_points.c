a796c9ec12fba963e2f8ac3ec870cc81ef792304
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-22 17:16:02 +0900

    solve

--- exercism/c/saddle-points/src/saddle_points.c
+++ exercism/c/saddle-points/src/saddle_points.c
@@ -1,1 +1,48 @@
 #include "saddle_points.h"
+
+saddle_points_t *saddle_points(int row, int column, uint8_t *matrix) {
+    int *row_max = malloc(sizeof(int) * row);
+    for (int i = 0; i < row; i++) {
+        row_max[i] = 0;
+    }
+    int *column_min = malloc(sizeof(int) * column);
+    for (int i = 0; i < column; i++) {
+        column_min[i] = 1000000;
+    }
+    for (int index = 0; index < row * column; index++) {
+        int value = matrix[index];
+        int row_index = index / column;
+        int column_index = index % column;
+        row_max[row_index] = get_max_value(row_max[row_index], value);
+        column_min[column_index] = get_min_value(column_min[column_index], value);
+    }
+    saddle_points_t *actual;
+    actual = malloc(sizeof(saddle_points_t));
+    actual->count = 0;
+    actual->points = malloc(sizeof(saddle_point_t *) * 1024);
+    for (int row_index = 0; row_index < row; row_index++) {
+        for (int column_index = 0; column_index < column; column_index++) {
+            if (row_max[row_index] == column_min[column_index]) {
+                saddle_point_t point = (saddle_point_t){
+                    .row = row_index + 1,
+                    .column = column_index + 1,
+                };
+                actual->points[actual->count++] = point;
+            }
+        }
+    }
+    return actual;
+}
+
+void free_saddle_points(saddle_points_t *actual) {
+    free(actual->points);
+    free(actual);
+}
+
+int get_max_value(int value, int base) {
+    return value > base ? value : base;
+}
+
+int get_min_value(int value, int base) {
+    return value < base ? value : base;
+}
--- exercism/c/saddle-points/src/saddle_points.h
+++ exercism/c/saddle-points/src/saddle_points.h
@@ -1,4 +1,31 @@
 #ifndef SADDLE_POINTS_H
 #define SADDLE_POINTS_H
 
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdint.h>
+
+typedef struct saddle_point_t saddle_point_t;
+typedef struct saddle_points_t saddle_points_t;
+
+struct saddle_point_t {
+    int row;
+    int column;
+};
+
+struct saddle_points_t {
+    int count;
+    saddle_point_t *points;
+};
+
+saddle_points_t *saddle_points(int row, int column, uint8_t *matrix);
+void free_saddle_points(saddle_points_t *actual);
+int get_max_value(int value, int base);
+int get_min_value(int value, int base);
+
 #endif
