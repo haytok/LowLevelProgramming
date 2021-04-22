cc432e6f54d46f19d147aff5840498826c1265ef
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-23 05:16:35 +0900

    solve

--- exercism/c/nth-prime/src/nth_prime.c
+++ exercism/c/nth-prime/src/nth_prime.c
@@ -1,1 +1,29 @@
 #include "nth_prime.h"
+
+uint32_t nth(uint32_t n) {
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 2;
+    }
+
+    int i, j, k;
+    int counts = 1;
+	for (i = 3; i <= 10000000; i += 2) {
+		k = 0;
+		for (j = 3 ; j <= sqrt(i); j += 2) {
+			if (i % j == 0) {
+				k = 1;
+				break;
+			}
+		}
+
+		if (k == 0) {
+            counts++;
+        }
+        if (counts == (int)n) {
+            return i;
+        }
+	}
+    return -1;
+}
--- exercism/c/nth-prime/src/nth_prime.h
+++ exercism/c/nth-prime/src/nth_prime.h
@@ -2,6 +2,14 @@
 #define NTH_PRIME_H
 
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <math.h>
 
 uint32_t nth(uint32_t n);
 
