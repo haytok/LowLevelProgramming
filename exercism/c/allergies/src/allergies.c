5a548abe66d7e5318d690b8748f98c2d9be18f17
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-22 18:49:07 +0900

    solve

--- exercism/c/allergies/src/allergies.c
+++ exercism/c/allergies/src/allergies.c
@@ -1,1 +1,38 @@
 #include "allergies.h"
+
+bool is_allergic_to(allergen_t allergen, int value) {
+    int *data;
+    data = malloc(sizeof(int) * LENGTH);
+    create_data(value, data);
+    return data[allergen] == 1;
+}
+
+allergen_list_t get_allergens(int value) {
+    int *data;
+    data = malloc(sizeof(int) * LENGTH);
+    create_data(value, data);
+
+    allergen_list_t allergen_list;
+    allergen_list.count = 0;
+    for (int i = 0; i < LENGTH; i++) {
+        if (i < ALLERGEN_COUNT && data[i]) {
+            allergen_list.allergens[i] = true;
+            allergen_list.count++;
+        }
+    }
+
+    return allergen_list;
+}
+
+void create_data(int value, int *data) {
+    int n = 0;
+    int bit = 1;
+    int length = (int)(log10((double)value) / log10((double)2));
+    while (length >= 0) {
+        // 下の桁から計算
+        bit = (value >> n) & 1;
+        data[n] = bit;
+        n++;
+        length--;
+    }
+}
--- exercism/c/allergies/src/allergies.h
+++ exercism/c/allergies/src/allergies.h
@@ -1,7 +1,17 @@
 #ifndef ALLERGIES_H
 #define ALLERGIES_H
 
+#include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdint.h>
+#include <math.h>
+
+#define LENGTH 32
 
 typedef enum {
    ALLERGEN_EGGS = 0,
@@ -19,5 +29,9 @@ typedef struct {
    int count;
    bool allergens[ALLERGEN_COUNT];
 } allergen_list_t;
+
+bool is_allergic_to(allergen_t allergen, int value);
+allergen_list_t get_allergens(int value);
+void create_data(int value, int *data);
 
 #endif
