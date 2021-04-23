29666542fbe99fc700a2ac7ae7a25ce62619a905
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-24 03:35:49 +0900

    solve

--- exercism/c/largest-series-product/src/largest_series_product.c
+++ exercism/c/largest-series-product/src/largest_series_product.c
@@ -1,1 +1,26 @@
 #include "largest_series_product.h"
+
+int64_t largest_series_product(char *digits, size_t span) {
+    if ((int)span < 0) {
+        return -1;
+    }
+    int ans = -1;
+    int end_index = strlen(digits) - (int)span + 1;
+    for (int i = 0; i < end_index; i++) {
+        int value = 1;
+        for (int j = i; j < i + (int)span; j++) {
+            if (isalpha(digits[j])) {
+                return -1;
+            }
+            char *buf;
+            buf = malloc(sizeof(char) * 1);
+            sprintf(buf, "%c", digits[j]);
+            value *= atoi(buf);
+        }
+        if (ans < value) {
+            ans = value;
+        }
+        value = 1;
+    }
+    return (int64_t)ans;
+}
--- exercism/c/largest-series-product/src/largest_series_product.h
+++ exercism/c/largest-series-product/src/largest_series_product.h
@@ -2,6 +2,13 @@
 #define LARGEST_SERIES_PRODUCT_H
 
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
 #include <stdint.h>
 
 int64_t largest_series_product(char *digits, size_t span);
