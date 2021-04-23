b3766c816c9dc355bfa10d3bb8a8d2e660d87c9e
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-23 20:00:34 +0900

    solve

--- exercism/c/sublist/src/sublist.c
+++ exercism/c/sublist/src/sublist.c
@@ -1,1 +1,52 @@
 #include "sublist.h"
+
+#define ARRAY_COUNT(array) (int)(sizeof(array) / sizeof(array[0]))
+
+comparison_result_t check_lists(int *list_to_compare, int *base_list,
+                                size_t list_to_compare_element_count,
+                                size_t base_list_element_count) {
+    if (list_to_compare_element_count == base_list_element_count) {
+        if (list_to_compare_element_count == 0 && base_list_element_count == 0) {
+            return EQUAL;
+        }
+
+        for (int i = 0; i < (int)list_to_compare_element_count; i++) {
+            if (list_to_compare[i] != base_list[i]) {
+                return UNEQUAL;
+            }
+        }
+        return EQUAL;
+    } else if (list_to_compare_element_count < base_list_element_count) {
+        if (list_to_compare_element_count == 0) {
+            return SUBLIST;
+        }
+
+        int end = base_list_element_count - list_to_compare_element_count + 1;
+        return is_exist_in_array(list_to_compare, base_list, end) ? SUBLIST : UNEQUAL;
+    } else if (list_to_compare_element_count > base_list_element_count) {
+        if (base_list_element_count == 0) {
+            return SUPERLIST;
+        }
+
+        int end = list_to_compare_element_count - base_list_element_count + 1;
+        return is_exist_in_array(base_list, list_to_compare, end) ? SUPERLIST : UNEQUAL;
+    }
+    return UNEQUAL;
+}
+
+bool is_exist_in_array(int *compare, int *base, int end) {
+    for (int i = 0; i < end; i++) {
+        int j;
+        bool flag = true;
+        for (j = 0; j < ARRAY_COUNT(compare); j++) {
+            if (compare[j] != base[i + j]) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            return true;
+        }
+    }
+    return false;
+}
--- exercism/c/sublist/src/sublist.h
+++ exercism/c/sublist/src/sublist.h
@@ -2,6 +2,14 @@
 #define SUBLIST_H
 
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdint.h>
 
 typedef enum {
    EQUAL,
@@ -13,5 +21,6 @@ typedef enum {
 comparison_result_t check_lists(int *list_to_compare, int *base_list,
                                 size_t list_to_compare_element_count,
                                 size_t base_list_element_count);
+bool is_exist_in_array(int *compare, int *base, int end);
 
 #endif
