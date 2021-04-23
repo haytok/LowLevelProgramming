7ee64230fdc7d18996cc74f3d80ff3771f8ffbd6
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-24 03:50:48 +0900

    solve

--- exercism/c/scrabble-score/src/scrabble_score.c
+++ exercism/c/scrabble-score/src/scrabble_score.c
@@ -1,1 +1,14 @@
 #include "scrabble_score.h"
+
+unsigned int score(const char *word) {
+    int score[LENGTH] = {
+        1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10
+    };
+    int length = strlen(word);
+    int ans = 0;
+    for (int i = 0; i < length; i++) {
+        int index = tolower(word[i]) - 'a';
+        ans += score[index];
+    }
+    return (unsigned int)ans;
+}
--- exercism/c/scrabble-score/src/scrabble_score.h
+++ exercism/c/scrabble-score/src/scrabble_score.h
@@ -1,6 +1,17 @@
 #ifndef SCRABBLE_SCORE_H
 #define SCRABBLE_SCORE_H
 
+#define LENGTH 26
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdint.h>
+
 unsigned int score(const char *word);
 
 #endif
