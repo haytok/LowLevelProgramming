7d21154af2d7e8d98afdd4670736518ed1619307
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-04-30 23:02:51 +0900

    write log

---
+++ editor/log/test_12.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void progress() {
+    static int pos = 0;
+    static char mark[] = {'-', '\\', '|', '/'};
+    fprintf(stderr, "\r%c", mark[pos]);
+    pos = (pos + 1) % sizeof(mark);
+}
+
+int main(void) {
+    int i, j;
+    fprintf(stderr, "0%%       50%%       100%%\n");
+    fprintf(stderr, "+---------+---------+\n");
+    for (i = 0; i <= 100; i++) {
+        for (j = 0; j < i / 5 + 1; j++) {
+            fprintf(stderr, "##");
+        }
+        fprintf(stderr, "\n");
+        fprintf(stderr, "%3d%%\n", i);
+        usleep(100000);
+        fprintf(stderr, "\033[2A");
+        fprintf(stderr, "\033[?25l");
+        fprintf(stderr, "\033[H");
+    }
+    printf("\nfinish!!!\n");
+}
