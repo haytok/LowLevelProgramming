66af42460063ba2305ab76d9e18878c276f6b5e0
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-06-20 19:01:28 +0900

    fix

--- log/20210508/test_6.c
+++ log/20210508/test_6.c
@@ -1,9 +1,9 @@
 #define _GNU_SOURCE
 #include <unistd.h>
 #include <sys/syscall.h>
-#include <bits/syscall.h>
+#include <asm/unistd_64.h>
 
 int main(void) {
     char *buf = "Hello\n";
-    syscall(SYS_write, STDOUT_FILENO, buf, sizeof(buf));
+    syscall(__NR_write, STDOUT_FILENO, buf, sizeof(buf));
 }
